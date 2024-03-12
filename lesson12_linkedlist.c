dff8fdd9e63833c2955a28570e0c0561fad66540
luka.chalker <52194915+cyber-chalk@users.noreply.github.com>
2024-03-12 04:03:44 +0000

    half

--- lesson12_linkedlist.c
+++ lesson12_linkedlist.c
@@ -5,16 +5,25 @@ struct node {
   struct node *next;
 };
 
+
+void insertNode(struct node original, int val) {
+  struct node newNode;
+  newNode.data = val;
+  newNode.next = NULL;
+
+  original->next = &newNode;
+}
 int main() {
 
 struct node node1, node2, node3;
-    node1.data = 10;
-    node2.data = 20;
-    node3.data = 30;
+    node1.data = 1;
+    node2.data = 2;
+    node3.data = 3;
 
     node1.next = &node2;
     node2.next = &node3;
     node3.next = NULL; // Mark the end of the list
 
+    insertNode(node3, 4);
     return 0;
 }
