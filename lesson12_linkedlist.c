e2122eecd09f8c3ab20bab8f65c4676271be3531
luka.chalker <52194915+cyber-chalk@users.noreply.github.com>
2024-03-14 10:05:30 +1100

    Update lesson12_linkedlist.c

--- lesson12_linkedlist.c
+++ lesson12_linkedlist.c
@@ -1,29 +1,58 @@
 #include <stdio.h>
 
-struct node {
+struct _node {
   int data;
-  struct node *next;
+  struct _node *next;
 };
 
+typedef struct _node node;
 
-void insertNode(struct node original, int val) {
-  struct node newNode;
-  newNode.data = val;
-  newNode.next = NULL;
 
-  original->next = &newNode;
+void removeNode(node *head, int position) {
+  int i = 0;
+    while (head != NULL) {
+      if (i == position) break;
+      head = head->next;
+    }
+    
+
+}
+
+void insertNode(node* original, int val) {
+  // printf("%d", original);
+    /*1. check if the given prev_node is NULL */
+  if (original == NULL) {
+    printf("nuh");
+      return;
+  }
+ 
+    /* 2. allocate new node */
+  node *newNode;
+    /* 3. put in the data */
+  newNode->data = val;
+    /* 4. Make next of new node as next of prev_node */
+   newNode->next = original->next;
+ 
+    /* 5. move the next of prev_node as new_node */
+  original->next = newNode;
+
 }
 int main() {
 
-struct node node1, node2, node3;
-    node1.data = 1;
-    node2.data = 2;
-    node3.data = 3;
+ node *node1, *node2, *node3;
+    node1->data = 1;
+    node2->data = 2;
+    node3->data = 3;
 
-    node1.next = &node2;
-    node2.next = &node3;
-    node3.next = NULL; // Mark the end of the list
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = NULL; // Mark the end of the list
 
     insertNode(node3, 4);
+
+    node *head = node1; //&
+    while (head != NULL) head = head->next;
+    removeNode(head, 2);
+
     return 0;
 }
