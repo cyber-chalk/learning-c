f5ae7c586a43d6eb24341d3ed57f2f286f825ace
luka.chalker <52194915+cyber-chalk@users.noreply.github.com>
2024-03-14 13:03:37 +0000

    lesson12

--- lesson12_linkedlist.c
+++ lesson12_linkedlist.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 struct _node {
   int data;
   struct _node *next;
@@ -7,56 +7,64 @@ struct _node {
 
 typedef struct _node node;
 
-
-void removeNode(node *head, int position) {
-  int i = 0;
-    while (head != NULL) {
-      if (i == position) break;
-      head = head->next;
-    }
-    
-
+void traverse(node *head) {
+  while (head != NULL)
+  {
+    printf("The payload is: %d\n", head->data);
+    head = head->next;
+  }
+  printf("\n");
 }
 
-void insertNode(node *original, node* newNode, int val) {
-  // printf("%d", original);  
-  //printf("%d %d", newNode == NULL, newNode);
-  
-// newNode->data = val;
- if (newNode == NULL) {
- 	printf("yes");
-  //	newNode->next = NULL;
-  	//original->next = newNode;
-	return;
- }
-//  newNode->next = original->next;
-//  original->next = newNode;
+void removeNode(node *head, int position) {
+  if (head == NULL) return;
 
+  int i = 0;
+    while (head != NULL) {
+      i++;
+      if (i == position) break;
+      // printf("The payload is: %d\n", head->data);
+      head = head->next;
+    }
+
+  if (head == NULL && i > position) {printf("wamp wamp"); return;}
+  head->next = head->next->next; // Skip the node to be removed
+  head = NULL;
+}
+
+void insertNodeAtHead(node *head, node *ptr0) {
+  ptr0->next = head;
+  ptr0->data = head->data - 1;
+}
+
+void insertNode(node *original, node* newNode) {
+  newNode->data = original->data + 1;
+  newNode->next = NULL;
+  original->next = newNode;
 }
 int main() {
 
- node node1, node2, node3;
-    node1.data = 1;
-    node2.data = 2;
-    node3.data = 3;
+  node node1 = {1}, node2 = {2}, node3 = {3};
 
     node1.next = &node2;
     node2.next = &node3;
-    node3.next = NULL; 
+    node3.next = NULL;
 
-	node *newNode;
-	node *head = &node1; //&
-	while (head != NULL) {
-  	//printf("%d", head->data);
-  	head = head->next; 
-  	}
-  	
-    insertNode(head, newNode, 4);
+    node *head = &node3; //&
+    node newNode;
+    node *newPoint = &newNode;
 
-    
-  	
-  	
-   //emoveNode(head, 2);
+    insertNode(head, newPoint);
+    traverse(head = &node1);
 
+    node node0;
+    node *ptr0 = &node0;
+    head = &node1;
+    insertNodeAtHead(head, ptr0);
+    traverse(head = &node0);
+
+    head = &node0;
+    removeNode(head, 2);
+    traverse(head = &node0);
     return 0;
 }
