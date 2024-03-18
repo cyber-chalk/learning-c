62e82cd0387c239aa98c4552cd723371247180a0
luka.chalker <52194915+cyber-chalk@users.noreply.github.com>
2024-03-18 10:00:06 +0000

    lesson15

--- lesson15.c
+++ lesson15.c
@@ -10,14 +10,14 @@ size_t strlenn (char *str) {
 }
 
 void copyString(char *to, char *from) {
-	printf(*to);
+	// printf("%c", to[0]);
 	size_t length = strlenn(from);
 	
-	//if(*to[0] == NULL) *to = to[length];
-//	*to = {"\0"};
-//	for (i = 0; i < length; i++) {
-//		*to[i] = *from[i];
-//	}
+	if(*to != '\0') *to = to[length];
+	*to = '\0';
+	for (int i = 0; i < length; i++) {
+		to[i] = from[i];
+	}
 }
 
 
@@ -27,5 +27,6 @@ int main () {
 	char str1[10] = "hello";
 	char str2[10] = "world";
 	copyString(str2, str1);
+	printf("%s %s", str1, str2);
 	return 0;
 }
